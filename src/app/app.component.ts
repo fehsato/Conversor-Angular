599c69a8d2f0c7ca1861fbe9be24bd59312d4590
fehsato <fyonezawa14@gmail.com>
2023-04-11 17:38:55 -0300

    calculos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+ 
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conversao-angular';
+
+  valor1 = 0;
+  resp = '0';
+
+  centimetros(): void {
+    this.resp= `${this.valor1 * 100}`;
+  }
+
+  milimetros(): void {
+    this.resp= `${this.valor1 * 1000}`;
+  }
+
+  quilometros(): void {
+    this.resp= `${this.valor1 * 0.001}`;
+  }
 }
